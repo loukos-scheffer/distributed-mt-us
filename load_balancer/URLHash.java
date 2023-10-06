4a28922b3707ea8a6833ac72f703117c65bbdc90
Keshav Venkatesh Worathur <worathur@dh2020pc04.utm.utoronto.ca>
2023-10-06 14:49:25 -0400

    Squashed commit of the following:
    commit 2bc6c7418066c8119db4d1f5f301bc4a6b394cdf
    Merge: b144780 c65caf4
    Author: Keshav Venkatesh Worathur <worathur@dh2020pc04.utm.utoronto.ca>
    Date:   Fri Oct 6 14:46:15 2023 -0400
    
        Merge branch 'load_balancer' of https://mcsscm.utm.utoronto.ca:9999/git/409/23f/a1group09 into load_balancer
    
    commit b1447807f2b511180578b5c9e46a6fe0be5fa788
    Author: Keshav Venkatesh Worathur <worathur@dh2020pc04.utm.utoronto.ca>
    Date:   Fri Oct 6 14:44:46 2023 -0400
    
        Create orchestration scripts
    
    commit aed56906f787db5813f3dd09cfbf0ec9f49b0489
    Author: Keshav Venkatesh Worathur <worathur@dh2020pc04.utm.utoronto.ca>
    Date:   Fri Oct 6 14:44:09 2023 -0400
    
        Define admin protocol
    
    commit 9ff88df3e491a6ccadb318eec0c9caba3698f30e
    Author: Keshav Venkatesh Worathur <worathur@dh2020pc04.utm.utoronto.ca>
    Date:   Fri Oct 6 14:43:55 2023 -0400
    
        Create Connection Pool class
    
    commit 4bb9d8144f4c4a59139b1a73d489725be10e35da
    Author: Keshav Venkatesh Worathur <worathur@dh2020pc04.utm.utoronto.ca>
    Date:   Fri Oct 6 14:43:37 2023 -0400
    
        Create hash function update method
    
    commit 74e33c052594ed9b712e5a662c09b369b70f30c8
    Author: Keshav Venkatesh Worathur <worathur@dh2020pc04.utm.utoronto.ca>
    Date:   Fri Oct 6 14:42:55 2023 -0400
    
        Define class holding shared thread data and supported operations on the data
    
    commit 5c6b4fe120fd3c88ddf0b2b9ea91ebff7b6dbc66
    Author: Keshav Venkatesh Worathur <worathur@dh2020pc04.utm.utoronto.ca>
    Date:   Fri Oct 6 14:42:33 2023 -0400
    
        Refactor RequestHandler to work with ThreadData
    
    commit add23f029e2d9bc6320e3cbdbe29230f815979a2
    Author: Keshav Venkatesh Worathur <worathur@dh2020pc04.utm.utoronto.ca>
    Date:   Fri Oct 6 14:42:15 2023 -0400
    
        Move all shared data to ThreadData.java
    
    commit e9f6eb463c1bd2222bfd6fb616ef98aeeb2a7f79
    Author: Keshav Venkatesh Worathur <worathur@dh2020pc04.utm.utoronto.ca>
    Date:   Fri Oct 6 14:41:57 2023 -0400
    
        Create monitoring app
    
    commit 21ce3c7eb9caa194046e5b8902c02be0e634e85e
    Author: Keshav Venkatesh Worathur <worathur@dh2020pc04.utm.utoronto.ca>
    Date:   Mon Oct 2 20:41:09 2023 -0400
    
        Update admin protocol
    
    commit c65caf4df390a39694ce6b6556c23686cf0c34cd
    Author: Keshav Venkatesh Worathur <worathur@dh2020pc04.utm.utoronto.ca>
    Date:   Mon Oct 2 20:41:09 2023 -0400
    
        Update admin protocol

--- load_balancer/URLHash.java
+++ load_balancer/URLHash.java
@@ -16,47 +16,15 @@ public class URLHash{
 
     private int numPartitions;
 
-
-    public static void main(String args[]){
-
-        int numPartitions = Integer.valueOf(args[0]);
-        URLHash h = new URLHash(numPartitions);
-
-        
-        HashMap<Integer, Integer> countsById = new HashMap<Integer, Integer>();
-        int partId;
-        String shortURL;
-        BufferedReader f;
-
-        for (int i = 0; i < numPartitions; i++) {
-            countsById.put(i, 0);
-        }
-
-
-        try {
-            f = new BufferedReader(new FileReader("database.txt"));
-
-            String line = f.readLine();
-        
-            while (line != null) {
-                shortURL = line.split("\\s+")[0];
-                partId = h.hashDJB2(shortURL);
-                countsById.put(partId, countsById.get(partId) + 1);
-                line = f.readLine();
-            }
-
-            countsById.forEach((key, value) -> System.out.println(key + " " + value));
-        } catch (IOException e) {
-            System.out.println("Exception occurred: " + e.getMessage());
-        }
-
-    }
-
     public URLHash(int numPartitions) {
         this.numPartitions = numPartitions;
     }
 
-    int hashDJB2(String key){
+    public void setNumPartitions(int numPartitions) {
+        this.numPartitions = numPartitions;
+    }
+
+    public int hashDJB2(String key){
         long hash = 5381;
 
         for (int i = 0; i < key.length(); i++) {
